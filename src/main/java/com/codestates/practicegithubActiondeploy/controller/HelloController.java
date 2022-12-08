382ad4e4d506877ee20a6b81480d94f37985cc1b
sojeonglee <sojeonglee0125@gmail.com>
2022-12-08 11:07:58 +0900

    fix : controller 패키지 위치 오류 수정

--- src/main/java/com/codestates/practicegithubActiondeploy/controller/HelloController.java
+++ src/main/java/com/codestates/practicegithubActiondeploy/controller/HelloController.java
@@ -1,12 +1,11 @@
-package controller;
+package com.codestates.practicegithubActiondeploy.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
-    @GetMapping("/")
+    @GetMapping
     public String hello() {
         return "Hello World! Hello BE BootCamp";
     }
